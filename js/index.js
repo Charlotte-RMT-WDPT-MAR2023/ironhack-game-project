175ab672a66a1eddb15b0365d755e57aa2b3751b
Rinan <jandrewschnell@gmail.com>
2023-05-03 17:09:14 +0200

    generate word day 1

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const duckBG = new Image();
 duckBG.src = './images/Ducks.jpg';
+let frames = 0;
 let score = 0;
 const input = document.getElementById('typeHere');
+const wordsOnscreen = [];
 
 //Add event listener to the button
 window.onload = () => {
@@ -20,9 +22,50 @@ window.onload = () => {
 //Add event listener to the Input
 //Add functions to start game
 function kidsGame(){
+    generateWord();
+    frames++
+    
+    // for(i=0;i<wordsOnscreen.length;i++){
+    //     wordsOnscreen[i].move();
+    // }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(duckBG, 0, 0, canvas.width, canvas.height);
+    // for(i=0;i<wordsOnscreen.length;i++){
+    //     wordsOnscreen[0].draw();
+    // }
+    checkWord();
+    let animation = requestAnimationFrame(kidsGame);
+    if(score === 10){cancelAnimationFrame(animation);}
 }
 
+//Word Generator
+const makeWord = {
+    y: Math.floor(Math.random() * 300)+ 50,
+    x: canvas.width,
+    speed: -1,
+    i: Math.floor(Math.random() * (wordsKids.length -1)),
+    
+    move: function() {
+      this.x += this.speed;
+    },
+    draw: function() {
+        ctx.font = '36px serif';
+        ctx.fillStyle = 'white';
+        ctx.fillText(wordsKids[this.i].toUpperCase(), this.x, this.y);
+    },
+  }
+  
+  function generateWord(){
+if(frames % 60 === 0){
+    wordsOnscreen.unshift(makeWord);
+    wordsOnscreen[0].y = Math.floor(Math.random() * 300)+ 50,
+    wordsOnscreen[0].x = canvas.width;
+    wordsOnscreen[0].i = Math.floor(Math.random() * wordsKids.length -1);
+}
+  }
+
+
+//Function to check if word is on screen
 function checkWord(event){
     if(input.value === 'test'){
         input.value = '';
