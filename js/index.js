84885beb1c69b8e9832fc37b8426e844bc049544
Rinan <jandrewschnell@gmail.com>
2023-05-04 08:01:31 +0200

    running words

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ const duckBG = new Image();
 duckBG.src = './images/Ducks.jpg';
 let frames = 0;
 let score = 0;
+let difficulty = 0;
 const input = document.getElementById('typeHere');
 const wordsOnscreen = [];
 
@@ -14,15 +15,80 @@ window.onload = () => {
     document.getElementById('btn-kids').style.visibility = 'hidden';
     input.addEventListener('input', checkWord);
     kidsGame();
+    difficulty = 1;
     };
   };
+
+//Create Class for the Word Generator
+class WordGenerator{
+  constructor(height, color, word){
+    this.height = height;
+    this.color = color;
+    this.word = word;
+    this.x = canvas.width
+  }
+
+  update() {
+    ctx.font = '36px serif';
+    ctx.fillStyle = this.color;
+    ctx.fillText(this.word.toUpperCase(), this.x, this.height);
+  }
+
+}
+
+function moveWords(){
+  for (i = 0; i < wordsOnscreen.length; i++) {
+    wordsOnscreen[i].x += -1;
+    wordsOnscreen[i].update();
+    
+  }
+  //every 2.4 secs
+  if(frames % 120 === 0){
+  let height = Math.floor(Math.random() * (canvas.height-50));
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let color = `rgb(${red},${green},${blue})`;
+  let iWord = Math.floor(Math.random() * wordsKids.length)
+  let word = wordsKids[iWord];
+  wordsOnscreen.push(new WordGenerator(height, color, word));
+  }
+
+}
+
+
+//Word Generator
+// const makeWord = {
+//     y: Math.floor(Math.random() * 300)+ 50,
+//     x: canvas.width,
+//     speed: -1,
+//     i: Math.floor(Math.random() * (wordsKids.length -1)),
+    
+//     move: function() {
+//       this.x += this.speed;
+//     },
+//     draw: function() {
+//         ctx.font = '36px serif';
+//         ctx.fillStyle = 'white';
+//         ctx.fillText(wordsKids[this.i].toUpperCase(), this.x, this.y);
+//     },
+//   }
+// function generateWord(){
+//   if(frames % 60 === 0){
+//       wordsOnscreen.unshift(makeWord);
+//       wordsOnscreen[0].y = Math.floor(Math.random() * 300)+ 50,
+//       wordsOnscreen[0].x = canvas.width;
+//       wordsOnscreen[0].i = Math.floor(Math.random() * wordsKids.length -1);
+//   }
+//     }
 
 
 
 //Add event listener to the Input
 //Add functions to start game
 function kidsGame(){
-    generateWord();
+
+    // generateWord();
     frames++
     
     // for(i=0;i<wordsOnscreen.length;i++){
@@ -30,47 +96,25 @@ function kidsGame(){
     // }
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(duckBG, 0, 0, canvas.width, canvas.height);
+    moveWords();
     // for(i=0;i<wordsOnscreen.length;i++){
     //     wordsOnscreen[0].draw();
     // }
     checkWord();
     let animation = requestAnimationFrame(kidsGame);
-    if(score === 10){cancelAnimationFrame(animation);}
+    if(score === 4){
+      cancelAnimationFrame(animation);
+    alert('Program Working So far');}
 }
 
-//Word Generator
-const makeWord = {
-    y: Math.floor(Math.random() * 300)+ 50,
-    x: canvas.width,
-    speed: -1,
-    i: Math.floor(Math.random() * (wordsKids.length -1)),
-    
-    move: function() {
-      this.x += this.speed;
-    },
-    draw: function() {
-        ctx.font = '36px serif';
-        ctx.fillStyle = 'white';
-        ctx.fillText(wordsKids[this.i].toUpperCase(), this.x, this.y);
-    },
-  }
-  
-  function generateWord(){
-if(frames % 60 === 0){
-    wordsOnscreen.unshift(makeWord);
-    wordsOnscreen[0].y = Math.floor(Math.random() * 300)+ 50,
-    wordsOnscreen[0].x = canvas.width;
-    wordsOnscreen[0].i = Math.floor(Math.random() * wordsKids.length -1);
-}
-  }
+
 
 
 //Function to check if word is on screen
-function checkWord(event){
+function checkWord(){
     if(input.value === 'test'){
         input.value = '';
         score++;
+        document.getElementById('annex').innerHTML = `Score: ${score}`
     }
 }
-//Background to canvas
-//Base Game
